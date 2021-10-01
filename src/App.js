65045b59ee79f6f1e09b1c347167fcca787a38a0
CorruptedMonk7 <rayhan.uddin.aiub@gmail.com>
2021-10-02 00:22:28 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Spinner,Row, Card,Col } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import News from './Components/News/News'
+
 
 function App() {
+  const [news, setNews] = useState([]);
+  useEffect(() => {
+    fetch('https://newsapi.org/v2/everything?q=tesla&from=2021-09-01&sortBy=publishedAt&apiKey=802750a25776416383a2d6f562cc605f')
+      .then(res => res.json())
+      .then(data => setNews(data.articles))
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { news.length ===0 ?
+      <Spinner animation="grow" />
+      :
+        <Row xs={1} md={3} className="g-4">
+        {
+          news.map(nw => <News
+          news={nw}
+          ></News> )
+        }
+      </Row>}
+
     </div>
   );
 }
